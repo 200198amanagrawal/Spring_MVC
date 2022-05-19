436dea994faa1ec5a6663e5774fa768e54b4d7b2
amanagrawal <amanagrawal2098@gmail.com>
2022-05-20 00:28:13 +0530

    spring mvc 5 step creation

---
+++ src/java/controller/HomeController.java
@@ -0,0 +1,21 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    
+    //this is for home url
+    @RequestMapping("/home")
+    public String home()
+    {
+        return "home";
+    }
+    
+    @RequestMapping("/about")
+    public String about()
+    {
+        return "about";
+    }
+}
